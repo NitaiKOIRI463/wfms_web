b967b02090889d8b5a9c5cf3d6a17b295742243b
anjalisingh50 <anjalsingh5050@gmail.com>
2022-06-06 15:24:49 +0530

    hghg

--- application/controllers/Company.php
+++ application/controllers/Company.php
@@ -10,6 +10,13 @@
 
 		public function index()
 		{
+			$api = 'Company_reg/CompanyList';
+			$data = '';
+			$method = 'post';
+			$result = $this->CallAPI($api,$data,$method);			
+			$d['com_list'] = $result['data'];
+			// echo "<pre>";
+			// print_r($d['com_list']);die;
 			$d['v'] = 'companyList';
 			$this->load->view('templates',$d);
 		}
